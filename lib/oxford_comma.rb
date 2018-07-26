fd4eb9d850a2aa197f820ee9aa2b5ee3b493223b
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-26 23:16:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.size == 1
+  array.join
+  elsif array.size == 2
+  array.join(" and ")
+else
+  conjunction = array.pop
+  oxford_comma = array.join(", ").concat(", and #{conjunction}")
+end
 end
